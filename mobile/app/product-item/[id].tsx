122e01ec8f3d1b2b5dc63cb9823a7b097e32caab
Eugene <eugyneehunter@gmail.com>
2026-03-13 22:34:00 +0300

    feat: enhance ProductItemScreen layout and seller information display

--- mobile/app/product-item/[id].tsx
+++ mobile/app/product-item/[id].tsx
@@ -74,40 +74,40 @@ const ProductItemScreen = () => {
             <ScrollView className='flex flex-col w-full '>
                 <Image source={{ uri: imageUrl }} className='w-full h-[450px]' resizeMode='cover' />
 
-            <View className='flex flex-col w-full p-2'>
-                <View className='flex flex-col w-full gap-2 p-2 mt-3'>
-                    <Text className='text-4xl text-black font-display-bold'>{title}</Text>
-                    <Text className='text-3xl text-primary font-display-bold'>Ksh {price}
-                        <Text className='ml-3 text-lg text-gray-500 font-display-medium'> Final Price</Text></Text>
-                </View>
+                <View className='flex flex-col w-full p-2'>
+                    <View className='flex flex-col w-full gap-2 p-2 mt-3'>
+                        <Text className='text-4xl text-black font-display-bold'>{title}</Text>
+                        <Text className='text-3xl text-primary font-display-bold'>Ksh {price}
+                            <Text className='ml-3 text-lg text-gray-500 font-display-medium'> Final Price</Text></Text>
+                    </View>
 
                     <View className='flex-row justify-between w-full gap-2 p-4 mt-3 bg-white shadow-lg rounded-2xl'>
-                    <View className='flex-col gap-1'>
-                        <Image source={{ uri: sellerAvatar }} className='rounded-full w-14 h-14' />
-                    </View>
+                        <View className='flex-col gap-1'>
+                            <Image source={{ uri: sellerAvatar }} className='rounded-full w-14 h-14' />
+                        </View>
 
-                    <View className='flex-col flex-1 gap-1 ml-5'>
-                        <Text className='text-3xl text-gray-900 font-display-semibold'>{sellerName}
-                            {sellerVerified === "true" && (
-                                <MaterialIcons name="verified" size={20} color="#3b82f6" className='mt-1 ml-2' />
-                            )}
-                        </Text>
-                        <Text className={`text-xl font-display-semibold ${sellerVerified === "true" ? "text-primary" : "text-gray-500"}`} >
-                            {sellerVerified === "true" ? `Verified Seller` : `Unverified Seller`}
-                        </Text>
-                    </View>
-
-                    <View className='flex-col items-end gap-1 p-1'>
-                        <View className='flex-row gap-2 p-1 mr-2 bg-yellow-100 rounded-lg'>
-                            <Ionicons name="star" size={16} color="#fbbf24" />
-                                <Text className='text-orange-900 text-md font-display-semibold' numberOfLines={1}>
-                                {sellerRating}
+                        <View className='flex-col flex-1 gap-1 ml-5'>
+                            <Text className='text-3xl text-gray-900 font-display-semibold'>{sellerName}
+                                {sellerVerified === "true" && (
+                                    <MaterialIcons name="verified" size={20} color="#3b82f6" className='mt-1 ml-2' />
+                                )}
+                            </Text>
+                            <Text className={`text-xl font-display-semibold ${sellerVerified === "true" ? "text-primary" : "text-gray-500"}`} >
+                                {sellerVerified === "true" ? `Verified Seller` : `Unverified Seller`}
                             </Text>
                         </View>
-                        <Text className='text-sm italic text-gray-500 font-display'>Responds in &lt; 1 hour</Text>
 
+                        <View className='flex-col items-end gap-1 p-1'>
+                            <View className='flex-row gap-2 p-1 mr-2 bg-yellow-100 rounded-lg'>
+                                <Ionicons name="star" size={16} color="#fbbf24" />
+                                <Text className='text-orange-900 text-md font-display-semibold' numberOfLines={1}>
+                                    {sellerRating}
+                                </Text>
+                            </View>
+                            <Text className='text-sm italic text-gray-500 font-display'>Responds in &lt; 1 hour</Text>
+
+                        </View>
                     </View>
-                </View>
 
                     <View className='flex-row justify-between w-full gap-1 p-2 mt-3'>
 
@@ -149,13 +149,21 @@ const ProductItemScreen = () => {
                             <Text className='text-3xl text-white font-display-bold'>{distance} km away</Text>
                         </View>
                     </View>
-
-                    <View>
-
-                    </View>
-
                 </View>
+
             </ScrollView>
+
+            <View className='fixed bottom-0 flex-row w-full gap-2 p-2 px-5 bg-white'>
+                <Pressable onPress={() => router.navigate('/chats')}
+                    className='items-center justify-center px-4 py-3 border border-gray-400 w-[30%] rounded-2xl'>
+                    <Ionicons name="chatbubble-ellipses-outline" size={24} color="gray" />
+                </Pressable>
+                <Pressable onPress={() => console.log(`Product ${id} : ${title} pressed`)}
+                    className='items-center justify-center px-8 py-3 w-[70%] rounded-2xl bg-primary'>
+                    <Text className='text-xl text-white font-display-bold'>Buy Now</Text>
+                </Pressable>
+            </View>
+
         </SafeAreaView>
     )
 }
