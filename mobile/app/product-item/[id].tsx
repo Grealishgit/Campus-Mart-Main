2a051370c2f94984bf7f8eecc26cff20184cf6bf
Eugene <eugyneehunter@gmail.com>
2026-03-13 21:11:40 +0300

    feat: enhance ProductItemScreen with seller details and pricing information

--- mobile/app/product-item/[id].tsx
+++ mobile/app/product-item/[id].tsx
@@ -2,7 +2,7 @@ import { View, Text, Pressable, Image } from 'react-native'
 import React, { useState } from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 
 const ProductItemScreen = () => {
@@ -73,6 +73,42 @@ const ProductItemScreen = () => {
                 </View>
 
             </View>
+            </View>
+
+            <View className='flex flex-col w-full p-2'>
+                <View className='flex flex-col w-full gap-2 p-2 mt-3'>
+                    <Text className='text-4xl text-black font-display-bold'>{title}</Text>
+                    <Text className='text-3xl text-primary font-display-bold'>Ksh {price}
+                        <Text className='ml-3 text-lg text-gray-500 font-display-medium'> Final Price</Text></Text>
+                </View>
+
+                <View className='flex-row justify-between w-full gap-2 p-2 mt-3 rounded-full shadow'>
+                    <View className='flex-col gap-1'>
+                        <Image source={{ uri: sellerAvatar }} className='rounded-full w-14 h-14' />
+                    </View>
+
+                    <View className='flex-col flex-1 gap-1 ml-5'>
+                        <Text className='text-3xl text-gray-900 font-display-semibold'>{sellerName}
+                            {sellerVerified === "true" && (
+                                <MaterialIcons name="verified" size={20} color="#3b82f6" className='mt-1 ml-2' />
+                            )}
+                        </Text>
+                        <Text className={`text-xl font-display-semibold ${sellerVerified === "true" ? "text-primary" : "text-gray-500"}`} >
+                            {sellerVerified === "true" ? `Verified Seller` : `Unverified Seller`}
+                        </Text>
+                    </View>
+
+                    <View className='flex-col items-end gap-1 p-1'>
+                        <View className='flex-row gap-2 p-1 mr-2 bg-yellow-100 rounded-lg'>
+                            <Ionicons name="star" size={16} color="#fbbf24" />
+                            <Text className='text-sm text-gray-500 font-display' numberOfLines={1}>
+                                {sellerRating}
+                            </Text>
+                        </View>
+                        <Text className='text-sm italic text-gray-500 font-display'>Responds in &lt; 1 hour</Text>
+
+                    </View>
+                </View>
             </View>
 
         </SafeAreaView>
